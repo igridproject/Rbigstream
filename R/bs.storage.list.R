967ed7a973752bfcdc0aebb7169e63a25e3f03be
kritpunpreuk <33280952+kritpunpreuk@users.noreply.github.com>
2018-05-18 16:49:23 +0700

    split function to seperate files

---
+++ R/bs.storage.list.R
@@ -0,0 +1,39 @@
+# -------------------------------------------------------------------------------------------
+# list all storages of default connection / given connection.
+
+#' Show storage list
+#' @description
+#' Given storage list from current connection setup or given connection
+#'
+#' @param conn Bigstream connection (No need if already call bs.connect() )
+#'
+#' @return Storage list in give Bigstream connection
+#'
+#' @examples
+#' host <- "http://sample.bigstream.io"
+#' port <- 19080
+#' storage_name <- "sample.sensordata"
+#' token <- "token"
+#' conn <- bs.connect(host, port, token)
+#' storage.list()
+#'
+#' or
+#'
+#' storage.list(conn)
+#' @export
+storage.list <- local(
+  function(conn = NULL) {
+    url <- ""
+    if(!is.null(conn))
+      url <- conn
+    else {
+      if(is.null(bs.active.url))
+        stop(bs.no.url)
+      url <- bs.active.url
+    }
+    if(!RCurl::url.exists(url))
+      stop("Cannot connnect to Bigstream via ", url)
+    cat("call Bigstreram API -> ",url,"\n")
+    jsonlite::read_json(url,simplifyVector = TRUE)
+  }
+, env = BS.env)
---
+++ R/bs.storage.stat.R
@@ -0,0 +1,24 @@
+#' Get storage information
+#'
+#' @param storage_name storage name
+#'
+#' @return Storage information
+#'
+#' @examples
+#' host <- "http://sample.bigstream.io"
+#' port <- 19080
+#' storage_name <- "sample.sensordata"
+#' token <- "token"
+#' conn <- bs.connect(host, port, token)
+#' storage.stat(storage_name)
+#' @export
+storage.stat <- local(
+  function(storage_name) {
+    if(is.null(bs.active.url))
+      stop(bs.no.url)
+    bs.active.url <- paste(bs.active.url, storage_name, "stats",sep = "/")
+    if(!RCurl::url.exists(bs.active.url))
+      stop("Cannot connnect to Bigstream via ", url)
+    jsonlite::read_json(bs.active.url,simplifyVector = TRUE)
+  }
+, env = BS.env)
